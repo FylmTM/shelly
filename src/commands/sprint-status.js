42eb66ff9b12095808ab97f82de263e6b018dd04
Dmitry Vrublevsky <dmitry@vrublevsky.me>
2018-10-08 23:24:45 +0100

    Add stub of sprint status

--- src/commands/sprint-status.js
+++ src/commands/sprint-status.js
@@ -1,13 +1,58 @@
 const config = require('../config');
 const errorExit = require('../util/errorExit');
 
+/**
+ * - Points done: 5/20 (20%)
+ * - Days left: 4 (20%)
+ * - Burndown chart
+ * - Blocked stories
+ * - Stories in progress
+ *  - ID, Title, Assignee
+ * - People without stories
+ */
 module.exports = () => {
   console.log(
     JSON.stringify({
       attachments: [
         {
-          text: 'Empty sprint status',
+          title: 'Team A Sprint 2',
+          title_link: 'https://example.com/',
+          color: '#738694',
+          fields: [
+            {
+              title: 'Points done',
+              value: '5/20 (20%)',
+              short: true,
+            },
+            {
+              title: 'Days left',
+              value: '4 (40%)',
+              short: true,
+            },
+          ],
+        },
+        {
+          title: 'Burdown',
+          title_link: 'https://example.com/',
+          color: '#738694',
+          image_url:
+            'https://luis-goncalves.com/content/uploads/2017/04/chart1.png',
+        },
+        {
+          title: 'Blocked Stories',
+          title_link: 'https://example.com/',
+          color: '#c23030',
+          text: `
+          <http://example.com|PROJ-2 (Subsystem A task 2)>
+          `.trim(),
+        },
+        {
+          title: 'Stories in progress',
+          title_link: 'https://example.com/',
           color: '#0d8050',
+          text: `
+          <http://example.com|PROJ-1 (Subsystem A task 1)> (:male-technologist: Dmitry Vrublevsky)
+          `.trim(),
         },
       ],
     })
